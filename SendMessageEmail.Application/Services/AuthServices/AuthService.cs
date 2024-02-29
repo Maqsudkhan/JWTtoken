08540ca0fcad99a40c0819cd1eb5c81c9f50a0cf
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-03-01 02:58:08 +0500

    so far so good

--- SendMessageEmail.Application/Services/AuthServices/AuthService.cs
+++ SendMessageEmail.Application/Services/AuthServices/AuthService.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SendMessageEmail.Domain.Entities.Models.AuthModels;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.AccessControl;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,31 +28,65 @@ namespace SendMessageEmail.Application.Services.AuthServices
             {
                 return "Use will be Null";
             }
-            else if (user.Id == Guid.NewGuid())
-            {
-                return "User not found";
-            }
+
 
             if (UserExit(user))
             {
-                
-                var service = _configuration.GetSection("JWT"); //Appsettingisdan qiymatni ovolishni 1-usuli
+
+                /*var service = _configuration.GetSection("JWT"); //Appsettingisdan qiymatni ovolishni 1-usuli
                 var sekretKey = service["Secret"];
 
-                var key = _configuration["JWT : Secret"]; // 2-usul
+                var key = _configuration["JWT : Secret"]; // 2-usul*/
+
+                List<Claim> claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Role, user.Role),
+                    new Claim("UserId", user.Id.ToString()),
+                    new Claim("UserName", user.UserName),
+                    new Claim("CreatedDate", DateTime.UtcNow.ToString())
+                };
+
+                return await GenerateToken(claims);
             }
             else
             {
-                return "User unAuthorithe";
+                return "User unAuthorithe 401";
             }
-
-            throw new NotImplementedException();
         }
+
+        public async Task<string> GenerateToken(IEnumerable<Claim> additionalClaims)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var exDate = Convert.ToInt32(_configuration["JWT:ExpireDate"] ?? "22");
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, EpochTime.GetIntDate(DateTime.UtcNow).ToString(CultureInfo.InvariantCulture), ClaimValueTypes.Integer64)
+            };
+
+            if (additionalClaims?.Any() == true)
+                claims.AddRange(additionalClaims);
+
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddSeconds(exDate),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
 
         private bool UserExit(User user)
         {
-            var login = "Admin";
-            var password = "12345";
+            var login = "Maqsudkhan";
+            var password = "Maqsudkh@n";
             if (user.Login == login && user.Password == password)
             {
                 return true;
